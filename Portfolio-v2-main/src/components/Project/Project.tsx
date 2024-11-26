44d21bc3a1b9a46c6a2753ecd31d34e528ceafdb
Shay_Batonya <127010513+ShaiBatonya@users.noreply.github.com>
2024-11-26 16:09:53 +0200

    Fix TypeScript configuration

--- Portfolio-v2-main/src/components/Project/Project.tsx
+++ Portfolio-v2-main/src/components/Project/Project.tsx
@@ -9,15 +9,13 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import project1 from '../../assets/project1.png';
 import project2 from '../../assets/project2.png';
 import project3 from '../../assets/project3.png';
-import project4 from '../../assets/project4.png';
-import project5 from '../../assets/project5.png';
+
 
 const projectImages = [
   project1,
   project2,
   project3,
-  project4,
-  project5
+
 ];
 
 const projects = [
